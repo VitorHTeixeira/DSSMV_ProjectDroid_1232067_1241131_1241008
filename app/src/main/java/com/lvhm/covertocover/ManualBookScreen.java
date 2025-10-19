897c2ec8d8058d0f639f2e7521d6bb8da8e0302d
lucas <lucaspereirasantos8126@gmail.com>
2025-10-19 19:13:22 +0100

    Criação do primeiro esboço do fragment_overlay_review.xml

--- app/src/main/java/com/lvhm/covertocover/ManualBookScreen.java
+++ app/src/main/java/com/lvhm/covertocover/ManualBookScreen.java
@@ -4,16 +4,12 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.FrameLayout;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
-import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
-import androidx.fragment.app.FragmentManager;
-import androidx.fragment.app.FragmentTransaction;
 
-public class ManualBook extends Fragment {
+public class ManualBookScreen extends Fragment {
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater,
