261f45aa80ce15f2dcbc5cc9f2b285f24f5f9a48
lucas <lucaspereirasantos8126@gmail.com>
2025-10-19 19:13:22 +0100

    Criação do primeiro esboço do fragment_overlay_review.xml

--- app/src/main/java/com/lvhm/covertocover/BookScreen.java
+++ app/src/main/java/com/lvhm/covertocover/BookScreen.java
@@ -4,27 +4,27 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.FrameLayout;
+import android.widget.RelativeLayout;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
-public class MainScreen extends Fragment {
+public class BookScreen extends Fragment {
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater,
                              @Nullable ViewGroup container,
                              @Nullable Bundle savedInstanceState) {
 
-        View view = inflater.inflate(R.layout.fragment_main, container, false);
+        View view = inflater.inflate(R.layout.fragment_book, container, false);
 
-        FrameLayout add_book = view.findViewById(R.id.add_book_button);
-        add_book.setOnClickListener(v -> {
-            Fragment fragment = new ManualBookScreen();
+        RelativeLayout add_review = view.findViewById(R.id.review_button);
+        add_review.setOnClickListener(v -> {
+            Fragment fragment = new BookReviewOverlay();
             getParentFragmentManager()
                     .beginTransaction()
-                    .replace(R.id.fragment_container, fragment)
+                    .add(R.id.fragment_container, fragment)
                     .addToBackStack(null)
                     .commit();
         });
