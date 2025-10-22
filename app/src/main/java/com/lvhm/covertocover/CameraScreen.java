63850b69d7d3911c7b1fa8b6fbdb884c98d10dac
Miguel Silva <miguelangelorsilva1@gmail.com>
2025-10-22 23:36:14 +0100

    - Mudança da CameraScreen para usar CameraX e PreviewView

--- app/src/main/java/com/lvhm/covertocover/CameraScreen.java
+++ app/src/main/java/com/lvhm/covertocover/CameraScreen.java
@@ -2,22 +2,28 @@ package com.lvhm.covertocover;
 
 import android.os.Bundle;
 import android.view.LayoutInflater;
-import android.view.SurfaceHolder;
-import android.view.SurfaceView;
 import android.view.View;
 import android.view.ViewGroup;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
-import androidx.fragment.app.Fragment;
-
+import androidx.camera.core.Camera;
 import androidx.camera.core.CameraSelector;
 import androidx.camera.core.Preview;
 import androidx.camera.lifecycle.ProcessCameraProvider;
 
+import androidx.camera.view.PreviewView;
+import androidx.core.content.ContextCompat;
+import androidx.fragment.app.Fragment;
+
+import com.google.common.util.concurrent.ListenableFuture;
+
 import java.util.concurrent.ExecutionException;
 
 public class CameraScreen extends Fragment {
+    private PreviewView previewView;
+    private ListenableFuture<ProcessCameraProvider> cameraProviderFuture;
+
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater,
@@ -25,33 +31,26 @@ public class CameraScreen extends Fragment {
                              @Nullable Bundle savedInstanceState) {
 
         View view = inflater.inflate(R.layout.fragment_cam, container, false);
+        previewView = view.findViewById(R.id.camera_preview_view);
+        cameraProviderFuture = ProcessCameraProvider.getInstance(requireContext());
 
-        SurfaceView camera_preview = view.findViewById(R.id.camera_preview_surface);
-        camera_preview.getHolder().addCallback(new SurfaceHolder.Callback() {
-            @Override
-            public void surfaceChanged(@NonNull SurfaceHolder surfaceHolder, int format, int width, int height) {}
+        cameraProviderFuture.addListener(() -> {
+            try {
+                ProcessCameraProvider cameraProvider = cameraProviderFuture.get();
 
-            @Override
-            public void surfaceCreated(@NonNull SurfaceHolder surfaceHolder) {
-                try {
-                    ProcessCameraProvider camera_provider = ProcessCameraProvider.getInstance(getContext()).get();
-                    Preview preview = new Preview.Builder().build();
+                Preview preview = new Preview.Builder()
+                        .build();
 
-                    CameraSelector camera_selector = new CameraSelector.Builder()
-                            .requireLensFacing(CameraSelector.LENS_FACING_BACK)
-                            .build();
+                CameraSelector cameraSelector = new CameraSelector.Builder()
+                        .requireLensFacing(CameraSelector.LENS_FACING_BACK)
+                        .build();
+                preview.setSurfaceProvider(previewView.getSurfaceProvider());
+                Camera camera = cameraProvider.bindToLifecycle(this, cameraSelector, preview);
 
-                    preview.setSurfaceProvider((Preview.SurfaceProvider) camera_preview.getHolder().getSurface());
-
-                    camera_provider.bindToLifecycle(CameraScreen.this, camera_selector, preview);
-                } catch (ExecutionException | InterruptedException e) {
-                    e.printStackTrace();
-                }
+            } catch (ExecutionException | InterruptedException e) {
+                e.printStackTrace();
             }
-
-            @Override
-            public void surfaceDestroyed(@NonNull SurfaceHolder surfaceHolder) {}
-        });
+        }, ContextCompat.getMainExecutor(requireContext()));
 
 
         return view;
