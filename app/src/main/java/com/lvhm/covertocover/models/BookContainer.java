30771da0c40570ce6111533ce516fc5fb1b98632
lucas <lucaspereirasantos8126@gmail.com>
2025-10-23 11:28:32 +0100

    Criadas algumas das classes do modelo

---
+++ app/src/main/java/com/lvhm/covertocover/models/BookContainer.java
@@ -0,0 +1,82 @@
+package com.lvhm.covertocover.models;
+
+import java.util.ArrayList;
+
+public class BookContainer {
+    private ArrayList<Book> books;
+
+    private Book findBook(String isbn, String name) {
+        for(Book book : this.books) {
+            if(book.getISBN().equals(isbn) || book.getName().equals(name)) {
+                return book;
+            }
+        }
+        return null;
+    }
+    private ArrayList<Book> findReadBooks(boolean read) {
+        ArrayList<Book> read_books = new ArrayList<>();
+        for(Book book : this.books) {
+            if(book.getRead() == read) {read_books.add(book);}
+        }
+        return read_books;
+    }
+    private ArrayList<Book> findOnGoingBooks(boolean onGoing) {
+        ArrayList<Book> onGoing_books = new ArrayList<>();
+        for(Book book : this.books) {
+            if(book.getOnGoing() == onGoing) {onGoing_books.add(book);}
+        }
+        return onGoing_books;
+    }
+    private ArrayList<Book> findWishlistedBooks(boolean wishlisted) {
+        ArrayList<Book> wishlisted_books = new ArrayList<>();
+        for(Book book : this.books) {
+            if(book.getIsWishlisted() == wishlisted) {wishlisted_books.add(book);}
+        }
+        return wishlisted_books;
+    }
+
+
+
+    public ArrayList<Book> getBooks() {
+        return books;
+    }
+
+    public BookContainer() {
+        books = new ArrayList<>();
+    }
+    public BookContainer(ArrayList<Book> books) {
+        this.books = books;
+    }
+
+    public void addBook(Book book) {
+        if(findBook(book.getISBN(), book.getName()) != null) {
+            // throw exception
+            return;
+        }
+        books.add(book);
+    }
+    public void deleteBook(Book book) {
+        if(findBook(book.getISBN(), book.getName()) == null) {
+            // throw exception
+            return;
+        }
+        books.remove(book);
+    }
+    public void updateBook(Book book) {
+        if(findBook(book.getISBN(), book.getName()) == null) {
+            // throw exception
+            return;
+        }
+        deleteBook(book);
+        addBook(book);
+    }
+    public ArrayList<Book> getListReadBooks() {
+        return findReadBooks(true);
+    }
+    public ArrayList<Book> getListOnGoingBooks() {
+        return findOnGoingBooks(true);
+    }
+    public ArrayList<Book> getListWishlistedBooks() {
+        return findWishlistedBooks(true);
+    }
+}
