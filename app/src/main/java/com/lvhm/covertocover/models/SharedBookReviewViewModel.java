715f41ebeef678f8b538b75a3bcccbf5ac857943
lucas <lucaspereirasantos8126@gmail.com>
2025-10-31 20:10:02 +0000

    Criação do SharedBookReviewViewModel
    Adicionado o `SharedBookReviewViewModel` para gerir o estado compartilhado entre fragments. Este ViewModel permite:
    - Selecionar e observar um `Book` através de `LiveData`.
    - Notificar e observar quando uma review é atualizada.

---
+++ app/src/main/java/com/lvhm/covertocover/models/SharedBookReviewViewModel.java
@@ -0,0 +1,22 @@
+package com.lvhm.covertocover.models;
+
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.ViewModel;
+
+public class SharedBookReviewViewModel extends ViewModel {
+    private MutableLiveData<Book> selected_book = new MutableLiveData<>();
+    private MutableLiveData<Boolean> review_updated = new MutableLiveData<>();
+
+    public void selectBook(Book book) {
+        selected_book.setValue(book);
+    }
+
+    public LiveData<Book> getSelectedBook() {
+        return selected_book;
+    }
+
+    public void notifyReviewUpdated() {review_updated.setValue(true);}
+
+    public LiveData<Boolean> getReviewUpdated() {return review_updated;}
+}
